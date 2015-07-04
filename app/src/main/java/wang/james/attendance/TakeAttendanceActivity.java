f4f9bc7f9cb39011ec6503104c998080cb87587f
James Wang <jameswang9909@hotmail.com>
2015-07-04 14:29:01 -0400

    Add back button to TakeAttendanceActivity and ViewAttendanceActivity

--- app/src/main/java/wang/james/attendance/TakeAttendanceActivity.java
+++ app/src/main/java/wang/james/attendance/TakeAttendanceActivity.java
@@ -1,10 +1,12 @@
 package wang.james.attendance;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class TakeAttendanceActivity extends ActionBarActivity {
 
@@ -17,7 +19,6 @@ public class TakeAttendanceActivity extends ActionBarActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
@@ -37,6 +38,10 @@ public class TakeAttendanceActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/wang/james/attendance/ViewAttendanceActivity.java
+++ app/src/main/java/wang/james/attendance/ViewAttendanceActivity.java
@@ -1,16 +1,24 @@
 package wang.james.attendance;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class ViewAttendanceActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_attendance);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -30,6 +38,10 @@ public class ViewAttendanceActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
         }
 
         return super.onOptionsItemSelected(item);
