6a8a56c8315a73fa22137280365f1158f0e36dc5
James Wang <jameswang9909@hotmail.com>
2015-07-04 16:23:12 -0400

    Add setters and getters for the date

--- app/src/main/java/wang/james/attendance/Configuration.java
+++ app/src/main/java/wang/james/attendance/Configuration.java
@@ -4,29 +4,56 @@ package wang.james.attendance;
  * Created by james on 7/3/15.
  */
 public class Configuration {
+    private static final Configuration config = new Configuration();
     final int ID_LENGTH = 9;
     final String url = "https://stuypulse-attendance.appspot.com/";
     private String ADMIN_EMAIL = "";
     private String ADMIN_PASSWORD = "";
+    private int DAY = -1;
+    private int MONTH = -1;
+    private int YEAR = -1;
 
-    private static final Configuration config = new Configuration();
     public static Configuration getInstance() {
         return config;
-    }
-
-    public void setAdminEmail(String ADMIN_EMAIL) {
-        this.ADMIN_EMAIL = ADMIN_EMAIL;
-    }
-
-    public void setAdminPassword(String ADMIN_PASSWORD) {
-        this.ADMIN_PASSWORD = ADMIN_PASSWORD;
     }
 
     public String getAdminEmail() {
         return ADMIN_EMAIL;
     }
 
+    public void setAdminEmail(String ADMIN_EMAIL) {
+        this.ADMIN_EMAIL = ADMIN_EMAIL;
+    }
+
     public String getAdminPassword() {
         return ADMIN_PASSWORD;
+    }
+
+    public void setAdminPassword(String ADMIN_PASSWORD) {
+        this.ADMIN_PASSWORD = ADMIN_PASSWORD;
+    }
+
+    public int getDay() {
+        return DAY;
+    }
+
+    public void setDay(int DAY) {
+        this.DAY  = DAY;
+    }
+
+    public int getMonth() {
+        return MONTH;
+    }
+
+    public void setMonth(int MONTH) {
+        this.MONTH = MONTH;
+    }
+
+    public int getYear() {
+        return YEAR;
+    }
+
+    public void setYear(int YEAR) {
+        this.YEAR = YEAR;
     }
 }
