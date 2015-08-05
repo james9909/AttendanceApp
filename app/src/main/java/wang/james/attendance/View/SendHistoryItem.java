84c1b71866fc078c1deb92396d031a13656eb7fd
James Wang <jameswang9909@hotmail.com>
2015-08-05 15:12:04 -0400

    Implement parcelable to store in bundle

--- app/src/main/java/wang/james/attendance/View/SendHistoryItem.java
+++ app/src/main/java/wang/james/attendance/View/SendHistoryItem.java
@@ -1,9 +1,12 @@
 package wang.james.attendance.View;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by james on 8/4/15.
  */
-public class SendHistoryItem {
+public class SendHistoryItem implements Parcelable {
 
     private String id;
     private boolean valid;
@@ -13,11 +16,39 @@ public class SendHistoryItem {
         this.valid = valid;
     }
 
+    protected SendHistoryItem(Parcel in) {
+        id = in.readString();
+        valid = (boolean) in.readValue(null);
+    }
+
+    public static final Creator<SendHistoryItem> CREATOR = new Creator<SendHistoryItem>() {
+        @Override
+        public SendHistoryItem createFromParcel(Parcel in) {
+            return new SendHistoryItem(in);
+        }
+
+        @Override
+        public SendHistoryItem[] newArray(int size) {
+            return new SendHistoryItem[size];
+        }
+    };
+
     public String getId() {
         return id;
     }
 
     public boolean getValid() {
         return valid;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeValue(valid);
     }
 }
